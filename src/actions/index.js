d978172e49e3a4aabab5fd3408f36928cecf3321
josh <josh>
2017-04-26 14:31:18 +0800

    Initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const API_KEY = "145fff06e6712ab3f6d1e173a05bec18";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us&` + `appid=${API_KEY}`;
+	const request = axios.get(url)
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
